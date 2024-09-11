e9aed3b45fb007a2e3e8d8365b7ce7e5ea3c951a
Jasso Lopez Andres Emilio <zS22017027@estudiantes.uv.mx>
2024-09-10 18:10:49 -0600

    Clase 10-09: Principios SOLID - Dependency Inversion Principle

---
+++ src/Bloque1/SOLID/DIPZuzzetJordan/DIP/DocumentGenerator.java
@@ -0,0 +1,14 @@
+package Bloque1.SOLID.DIPZuzzetJordan.DIP;
+
+public class DocumentGenerator {
+    private InputReader reader = new InputReader();
+    private TxtFileCreator txt = new TxtFileCreator();
+
+    public void makeTxtFile() {
+        String content = reader.getContent();
+        String name = reader.getFileName();
+
+        this.txt.createFile(content, name);
+        System.out.println("Archivo creado exitosamente :)");
+    }
+}
---
+++ src/Bloque1/SOLID/DIPZuzzetJordan/DIP/Main.java
@@ -0,0 +1,11 @@
+package Bloque1.SOLID.DIPZuzzetJordan.DIP;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        DocumentGenerator documentGenerator = new DocumentGenerator();
+
+        documentGenerator.makeTxtFile();
+    }
+}
